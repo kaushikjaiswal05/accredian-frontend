ac53133457b3521d02415a60c77c50cfea4ebe5f
Kaushik Jaiswal <kaushikjaiswal024@gmail.com>
2024-07-20 12:02:51 +0530

    Ensure correct dependencies and build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom']
+    }
+  }
 });
